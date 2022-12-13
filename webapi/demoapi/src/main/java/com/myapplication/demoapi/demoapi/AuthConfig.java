fba5f930ecedb57efb217896ca6d067675151e58
mukund <mukund@utilizecore.com>
2022-12-13 16:59:24 -0700

    added cors and also added auth0

---
+++ webapi/demoapi/src/main/java/com/myapplication/demoapi/demoapi/AuthConfig.java
@@ -0,0 +1,19 @@
+package com.myapplication.demoapi.demoapi;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class AuthConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+}
--- webapi/demoapi/src/main/java/com/myapplication/demoapi/demoapi/Config.java
+++ webapi/demoapi/src/main/java/com/myapplication/demoapi/demoapi/Config.java
@@ -5,13 +5,13 @@ import com.myapplication.demoapi.demoapi.model.PersonRole;
 import com.myapplication.demoapi.demoapi.model.Roles;
 import com.myapplication.demoapi.demoapi.repository.UserRepository;
 import com.myapplication.demoapi.demoapi.repository.UserRoleRepository;
-import org.apache.catalina.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,6 +29,18 @@ public class Config {
                     new PersonRole(Roles.Admin, result)
             );
             roleRepository.saveAll(roles);
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000/")
+                        .allowedMethods("*");
+            }
         };
     }
 }
