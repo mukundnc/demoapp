ed6e05e1ac50dc499c19df0d1d9b854776a621e3
mukund <mukund@utilizecore.com>
2022-12-15 00:54:28 -0700

    removed books

--- graphql/src/resolver/index.ts
+++ graphql/src/resolver/index.ts
@@ -1,14 +1,9 @@
-import data from '../data';
 import dateScalar from '../schema/dateScalar';
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => data,
-        book: (_, { title }) => {
-            return data.find(x => x.title === title);
-        },
         persons: (_, __, { dataSources }) => dataSources.personsAPI.getPersons(),
         personRoles: (_, __, { dataSources }) => dataSources.personRolesAPI.getPersonRoles(),
     },
--- graphql/src/schema/index.ts
+++ graphql/src/schema/index.ts
@@ -1,11 +1,6 @@
 const typeDefs = `#graphql
   scalar Date
 
-  type Book {
-    title: String
-    author: String
-  }
-
   type Person {
     id: Int
     firstName: String
@@ -18,22 +13,20 @@ const typeDefs = `#graphql
 
   type PersonRole {
     id: Int
-    role: String
+    role: Roles
     person: Person
   }
 
-  #enum Roles{
-  #  Patient,
-  #  Doctor,
-  #  Admin
-  #}
+  enum Roles{
+    Patient,
+    Doctor,
+    Admin
+  }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
-    book(title: String!): Book,
     persons: [Person],
     personRoles: [PersonRole]
   }
